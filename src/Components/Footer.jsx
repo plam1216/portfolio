d8dc19e76c762836915688f30c65c160c446cdb3
Peter Lam <plam1216@gmail.com>
2022-11-03 03:31:03 -0400

    page footer

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="row">
+          <div className="col s6 offset-md-2">
+            <a href="https://www.linkedin.com/in/plam1216/">
+              <img src="https://imgur.com/iNQ3ami.png" alt="linkedin" className="social-icons" />
+            </a>
+            <a href="https://github.com/plam1216">
+              <img src="https://imgur.com/noZk2jh.png" alt="github" className="social-icons"
+                style={{ backgroundColor: 'white', borderRadius: '24px' }} />
+            </a>
+          </div>
+          <div className="col s1 offset-md-3">
+            © 2022 Peter Lam
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
