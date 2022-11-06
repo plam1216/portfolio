d6dc09a61dee3ce2a020852370305ab7dc29e72a
Peter Lam <plam1216@gmail.com>
2022-11-06 16:22:14 -0500

    remove strictmode

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App.js';
 
 import { BrowserRouter as Router } from 'react-router-dom';
+import './style.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <Router>
       <App />
     </Router>
-  </React.StrictMode>
+  // </React.StrictMode>
 );
