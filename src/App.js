dfc04e64c7c28e8e2d791f89c6a451498f5ed386
Peter Lam <plam1216@gmail.com>
2022-11-06 16:21:55 -0500

    add components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import Nav from "./Components/Nav.jsx"
+import Home from "./Pages/Home.jsx";
+import About from "./Pages/About.jsx";
+import Projects from "./Components/Projects.jsx";
+import Footer from "./Components/Footer.jsx";
+import { Route, Switch } from "react-router";
+import Contact from "./Components/Contact.jsx";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Nav />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+          <Projects />
+          <Contact />
+        </Route>
+        <Route>
+          <About exact path="/about" />
+        </Route>
+      </Switch>
+      <Footer />
+    </>
   );
 }
 
